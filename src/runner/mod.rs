05a3aa95686bac4afec9bdda8ff858be7c7cbf0a
Kamil Ciemniewski <kamil@ciemniew.ski>
2021-02-25 11:57:30 +0000

    Refactor the library code

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -1,21 +1,19 @@
+mod cgroups;
 mod service;
 
 use anyhow::{Context, Error, Result};
-use controlgroup::{
-    v1::{Builder, UnifiedRepr},
-    Pid,
-};
+use cgroups::create_cgroups;
+use controlgroup::Pid;
 use service::{
-    log_response, run_request, run_response, status_response, stop_response, LogRequest,
-    RunRequest, StatusRequest, StopRequest,
+    log_response::LogError,
+    run_response::RunError,
+    status_response::{StatusError, StatusResult},
+    stop_response::StopError,
+    LogRequest, RunRequest, StatusRequest, StopRequest,
 };
-use std::boxed::Box;
 use std::collections::HashMap;
-use std::convert::TryFrom;
-use std::path::PathBuf;
-use std::process::{Child, Command};
+use std::process::Command;
 use std::sync::RwLock;
-use std::thread;
 use uuid::Uuid;
 
 // todo: implement std::iter::Iterator for this stream
@@ -28,9 +26,9 @@ pub struct Runner {
 }
 
 impl Runner {
-    pub fn run(&mut self, request: &RunRequest) -> Result<String, run_response::Error> {
+    pub fn run(&mut self, request: &RunRequest) -> Result<String, RunError> {
         let id = Uuid::new_v4().to_string();
-        let mut cgroups = self.create_cgroups(request, &id)?;
+        let mut cgroups = create_cgroups(request, &id)?;
 
         // todo: 1. bring back disk minor and major as it was
         //       2. do the stdout and stderr redirection
@@ -46,36 +44,15 @@ impl Runner {
             })?
     }
 
-    pub fn stop(&mut self, _request: &StopRequest) -> Result<(), stop_response::Error> {
+    pub fn stop(&mut self, _request: &StopRequest) -> Result<(), StopError> {
         unimplemented!();
     }
 
-    pub fn status(
-        &mut self,
-        _request: &StatusRequest,
-    ) -> Result<status_response::StatusResult, status_response::Error> {
+    pub fn status(&mut self, _request: &StatusRequest) -> Result<StatusResult, StatusError> {
         unimplemented!();
     }
 
-    pub fn log(&mut self, _request: &LogRequest) -> Result<LogStream, log_response::Error> {
+    pub fn log(&mut self, _request: &LogRequest) -> Result<LogStream, LogError> {
         unimplemented!();
-    }
-
-    fn create_cgroups(&mut self, request: &RunRequest, id: &str) -> Result<UnifiedRepr> {
-        let mut builder = Builder::new(PathBuf::from(id));
-
-        if let Some(run_request::Memory::MaxMemory(max)) = request.memory {
-            builder = builder.memory().limit_in_bytes(max).done();
-        }
-
-        if let Some(run_request::Cpu::MaxCpu(max)) = request.cpu {
-            builder = builder.cpu().shares(max).done();
-        }
-
-        if let Some(run_request::Disk::MaxDisk(max)) = request.disk {
-            builder = builder.blkio().weight(u16::try_from(max)?).done();
-        }
-
-        Ok(builder.build()?)
     }
 }
--- src/runner/service.rs
+++ src/runner/service.rs
@@ -1,21 +1,37 @@
 tonic::include_proto!("service");
 
 macro_rules! impl_from_anyhow {
-    ($($for:path), +) => {
-        $(
-            impl<T> std::convert::From<T> for $for
-            where
-                T: std::convert::Into<anyhow::Error>,
-            {
-                fn from(error: T) -> $for {
-                    run_response::Error {
-                        description: error.into().to_string(),
-                        errors: Some(run_response::error::Errors::GeneralError(1)),
-                    }
+    ($for:path, $error_type:expr) => {
+        impl<T> std::convert::From<T> for $for
+        where
+            T: std::convert::Into<anyhow::Error>,
+        {
+            fn from(error: T) -> $for {
+                $for {
+                    description: error.into().to_string(),
+                    errors: Some($error_type(1)),
                 }
             }
-        )+
+        }
     };
 }
 
-impl_from_anyhow!(run_response::Error);
+impl_from_anyhow!(
+    run_response::RunError,
+    run_response::run_error::Errors::GeneralError
+);
+
+impl_from_anyhow!(
+    stop_response::StopError,
+    stop_response::stop_error::Errors::GeneralError
+);
+
+impl_from_anyhow!(
+    status_response::StatusError,
+    status_response::status_error::Errors::GeneralError
+);
+
+impl_from_anyhow!(
+    log_response::LogError,
+    log_response::log_error::Errors::GeneralError
+);
