57e15cc49eb123d72476a45b89d0a59f7098adfa
Kamil Ciemniewski <kamil@ciemniew.ski>
2021-03-02 08:36:08 +0000

    Add a basic server main skeleton

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,28 @@
 mod cli;
 mod runner;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use cli::server::Cli;
+use std::time::Duration;
 use structopt::StructOpt;
 
-fn main() -> Result<()> {
-    let _ = cli::server::Cli::from_args();
+#[derive(Default)]
+pub struct RunnerServer;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Cli::from_args();
+
+    start_server(args)
+        .await
+        .with_context(|| "Failed to run the runner server")?;
+
+    Ok(())
+}
+
+async fn start_server(_args: Cli) -> Result<()> {
+    // todo: implement me
+    tokio::time::sleep(Duration::from_millis(9999999999)).await;
 
     Ok(())
 }
